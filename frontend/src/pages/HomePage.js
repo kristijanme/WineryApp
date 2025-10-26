45d7b50f46720dc30fe4c2e756a25e8ee2d4175a
kristijanme <kmitrov71@yahoo.com>
2025-10-26 22:15:18 +0100

    Final changes

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./HomePage.css";
-import vineyard from "../assets/images/vineyard.jpg"; // ✅ Correct import path
+import vineyard from "../assets/images/vineyard.jpg"; 
 
 export default function HomePage() {
   return (
     <div
       className="homepage-hero"
-      style={{ backgroundImage: `url(${vineyard})` }} // ✅ Inline background image
+      style={{ backgroundImage: `url(${vineyard})` }} 
     >
       <div className="homepage-overlay">
         <h1 className="text-danger hero-title"> Welcome to Winery App</h1>
         <p className="lead hero-subtitle">
-          Use the navigation above to manage wines or users.
+          Use the navigation above to manage wines users or orders.
         </p>
       </div>
     </div>
